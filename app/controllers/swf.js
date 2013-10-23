7c290e1bb35346556f93fcca8cbfdb5a314ec3d1
J.P. <jp@makemodo.com>
2013-10-23 08:57:16 -0400

    Can how hover elements to play sound
    kind of messy

--- app/controllers/swf.js
+++ app/controllers/swf.js
@@ -7,64 +7,50 @@ window.SwfController = {
     
     game: function() {
         steroids.view.navigationBar.show("Game");
-        
-        
+                
         steroids.on('ready', function() {
             
-            console.log('steroids app path = ' + steroids.app.path); // on iOS ~= "applications/local/123456789"
-        
             document.addEventListener("touchmove", function(e) { 
                 // Disable user scrolling
                 e.preventDefault(); 
                 
                 // Play notes if currently over an element
-
-
             });
             
             document.addEventListener("deviceready",onDeviceReady,false);
             
                 // Helper console log method
                 function logJson(e) {
-                    var str = JSON.stringify(e, undefined, 2);
-                    console.log(str);
+                    var str = JSON.stringify(e, undefined, 2);                    
                 }
             
                 // Should return /www root
                 function getSteroidsAbsPath() {
                     return "file://" + steroids.app.absolutePath;            
                 }
-                
-                
+                                
                 function getSteroidsAppPath() {
                     return steroids.app.path;            
                 }
-                
-                
+                                
                 function getSoundPath() {
                     return '/sounds/sequence/';
                 }
-                
-                
+                                
                 function getInternetSoundsPath() {
                     return window.location.origin + "/sounds/sequence/";
-                }
-                
+                }                
                 
                 function assignColorsToSounds(fullSoundPath) {
-                    var colors = [
+					BUTTONS = $(".button");
+                    colors = [
                         'green', 
                         'yellow',
                         'blue',  
                         'red'   
-                    ];
+                    ];                                       
                     
-                    for(var x = 0; x < colors.length; x++ ) {
-                        // This should work if all colors are .wav files : 
-                        //colors[x] = createNewSoundObject(fullSoundPath + colors[x] + '.wav');
-                    }
-                    
-                    var soundsPath = getSoundPath();
+                    var soundsPath = "file://" + steroids.app.absolutePath + '/sounds/sequence/';
                     colors['green'] = createNewSoundObject(soundsPath + 'green.wav', 'green');
                     
                     colors['yellow'] = createNewSoundObject(soundsPath + 'yellow.wav', 'yellow');
@@ -75,32 +61,67 @@ window.SwfController = {
                     
                     return colors;
                 }
-
+               
                 // Returns a Media Object with error messaging
                 // Can use .play() and other Meda functions
                 // *param 'relSoundPath' is the relative path to the sound file to play.
                 function createNewSoundObject(relSoundPath, assetName) {
 
                     // Preload the audio asset for later use
-                    PGLowLatencyAudio.preloadAudio(assetName, relSoundPath, 1);
-                    /*
-                        return new Media("file://" + steroids.app.absolutePath + relSoundPath,
-                            // Success
-                            function() {  
-                                //console.log("playing sound from path : " + absSoundPath);
-                            },
-                            // Error
-                            function(e) { 
-                                alert(relSoundPath);
-                                var str = JSON.stringify(e, undefined,  2); 
-                                console.log(str);
-                            }
-                        );
-                    */
+                    return new Media(relSoundPath,
+                        // Success
+                        function() {  
+							$("#yellow").text("success");  
+							$("#" + assetName).removeClass("active");
+                            $(document).trigger("touchstart");                            
+                        },
+                        // Error
+                        function(e) { 
+                            //alert(relSoundPath);
+                            var str = JSON.stringify(e, undefined,  2);                             
+                        }
+                    );                    
                 }
+				
+				function playBoxHovers(e) {
+				
+					var touchedIds = [];
+					var touches = e.originalEvent.touches;
+										
+					for(var x = 0; x < touches.length; x++) {
+						var touch = e.originalEvent.touches[x];
+						var $el = $(document.elementFromPoint(touch.clientX, touch.clientY));
+						
+						$("#green").text(touch.clientX);
+						
+						touchedIds[x] = $el.attr("id");
+					}
+					
+					BUTTONS.each(function() {
+						var id = $(this).attr("id");
+						var isHovered = touchedIds.indexOf(id) != -1;
+						
+						$("#red").text(touchedIds[0] + " " + isHovered);
+						
+						if(isHovered) {
+							//alert(isHovered);
+							$("#blue").text($(this).hasClass("active"));
+							
+							if(!$(this).hasClass("active")) {
+								$(this).addClass("active");
+								colors[id].play();								
+							}
+							
+						}
+						else {
+							// $("#blue").text(id + " " + this).hasClass("active"));
+							$(this).removeClass("active");
+							colors[id].stop();
+						}										
+					});								
+				}
 
-                function onDeviceReady () {
-                                
+                function onDeviceReady () {                            
                     
                     var steroidsPath       = getSteroidsAbsPath();
                     var soundPath          = getSoundPath();
@@ -111,50 +132,21 @@ window.SwfController = {
                     var internetSoundsPath = getInternetSoundsPath();
                     
                     //var colors = assignColorsToSounds(fullSoundPath);
-                    var colors = assignColorsToSounds(internetSoundsPath);
-
-                    //TEST = createNewSoundObject("file://" + steroids.app.absolutePath + '/sounds/sequence/green.wav');
+                    var colors = assignColorsToSounds(internetSoundsPath);                
                     
-                    $(document).on('touchstart', '.sequence-container .button', function() {
-                        
-                        // Play the preloaded sound
-                        var sound = $(this).attr('id');
-                        PGLowLatencyAudio.play(sound);
-
-                        //colors[sound].play();
-                        
-
-                        $(this).addClass("active");
-                                            
+                    $(document).on('touchstart', '.sequence-container .button', function(e) {
+                        playBoxHovers(e);                                                                  
                     });
 
                     // Add touchmove listener, check coords
                     $(document).on('touchmove', '.sequence-container .button', function(e) {
-                        // Don't play if already playing
-                        if($(this).hasClass('active')) {
-                            return;
-                        }
-
-                        // Check if moved over an item that isn't already playing
-                        var touch = e.originalEvent.touches[0];
-                        var $el = $(document.elementFromPoint(touch.clientX, touch.clientY));
-
-                        if(!$el.hassClass("active")) {
-                            $el.addClass("active");    
-                            var sound = $(this).attr('id');
-                            PGLowLatencyAudio.play(sound);
-                        }
+                        playBoxHovers(e);                        
                     });
                     
                     // I think we may need to bind coordinates to stop playing here as well
-                    $(document).on('touchend', '.sequence-container .button', function() {
-                        var sound = $(this).attr('id');
-                        //colors[sound].stop();
-                        PGLowLatencyAudio.stop(sound);
-
-                        $(this).removeClass('active');
-                    });
-            
+                    $(document).on('touchend', '.sequence-container .button', function(e) {
+                        playBoxHovers(e);
+                    });           
                 }
         }); // End steroids ready
 
